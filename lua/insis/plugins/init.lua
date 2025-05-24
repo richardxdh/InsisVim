66ca61dfcd56a4ced2abdd9264bf7c63a0f61f77
Richard <richardxdh@gmail.com>
2025-05-24 15:09:28 +0800

    add noice.nvim

--- lua/insis/plugins/init.lua
+++ lua/insis/plugins/init.lua
@@ -445,6 +445,25 @@ local plugins = {
   --     require("telescope").setup({})
   --   end,
   -- },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("insis.plugins.noice")
+    end,
+  },
 }
 
 -- Rust
